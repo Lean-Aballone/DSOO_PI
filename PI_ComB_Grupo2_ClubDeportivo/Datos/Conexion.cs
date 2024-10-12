bb47e310f029cac4c5ae91d272677831af0a08cb
Lean-Aballone <l.aballone@gmail.com>
2024-10-12 18:23:33 -0300

    conexion SQL

---
+++ PI_ComB_Grupo2_ClubDeportivo/Datos/Conexion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PI_ComB_Grupo2_ClubDeportivo.Datos {
+    internal class Conexion {
+
+        // declaramos las variables
+        private string baseDatos;
+        private string servidor;
+        private string puerto;
+        private string usuario;
+        private string clave;
+        private static Conexion? con = null;
+        private Conexion(){
+            this.baseDatos = "ComB_Grupo2_ClubDeportivo";
+            this.servidor = "localhost";
+            this.puerto = "3306";
+            this.usuario = "root";
+            this.clave = "";
+        }
+
+        public MySqlConnection CrearConcexion() {
+            // instanciamos una conexion
+            MySqlConnection? cadena = new MySqlConnection();
+            // el bloque try permite controlar errores
+            try {
+                cadena.ConnectionString = "datasource=" + this.servidor +
+                ";port=" + this.puerto +
+                ";username=" + this.usuario +
+                ";password=" + this.clave +
+                ";Database=" + this.baseDatos;
+            }
+
+            catch (Exception ex) {
+                cadena = null;
+                throw;
+            }
+            return cadena;
+        }
+        public static Conexion getInstancia() {
+            if (con == null) // quiere decir que la conexion esta cerrada
+            {
+                con = new Conexion(); // se crea una nueva
+            }
+            return con;
+        }
+
+    }
+}
