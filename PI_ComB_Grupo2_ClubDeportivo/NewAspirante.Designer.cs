6d0856e6e912249648235b6b783953792bf42f5c
Lean-Aballone <l.aballone@gmail.com>
2024-10-15 12:50:22 -0300

    Ventana de inscripcion de nuevo socio

---
+++ PI_ComB_Grupo2_ClubDeportivo/NewAspirante.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace PI_ComB_Grupo2_ClubDeportivo {
+    partial class NewAspirante {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            label1 = new Label();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            buttonIngresar = new Button();
+            btnLimpiar = new Button();
+            buttonVolver = new Button();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            label7 = new Label();
+            label6 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 65F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(10, 0, 0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel3, 0, 2);
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel4, 0, 1);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(280, 0);
+            tableLayoutPanel2.Margin = new Padding(0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 3;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.Size = new Size(520, 450);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 50);
+            label1.Margin = new Padding(50, 50, 50, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 15);
+            label1.TabIndex = 0;
+            label1.Text = "INSCRIPCION DE SOCIO";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 3;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel3.Controls.Add(buttonIngresar, 0, 0);
+            tableLayoutPanel3.Controls.Add(btnLimpiar, 1, 0);
+            tableLayoutPanel3.Controls.Add(buttonVolver, 2, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(0, 305);
+            tableLayoutPanel3.Margin = new Padding(0, 0, 0, 45);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(520, 100);
+            tableLayoutPanel3.TabIndex = 1;
+            // 
+            // buttonIngresar
+            // 
+            buttonIngresar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonIngresar.Location = new Point(45, 52);
+            buttonIngresar.Margin = new Padding(45, 3, 3, 25);
+            buttonIngresar.Name = "buttonIngresar";
+            buttonIngresar.Size = new Size(75, 23);
+            buttonIngresar.TabIndex = 0;
+            buttonIngresar.Text = "INGRESAR";
+            buttonIngresar.UseVisualStyleBackColor = true;
+            buttonIngresar.Click += buttonIngresar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnLimpiar.Location = new Point(218, 52);
+            btnLimpiar.Margin = new Padding(45, 3, 3, 25);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(75, 23);
+            btnLimpiar.TabIndex = 1;
+            btnLimpiar.Text = "LIMPIAR";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // buttonVolver
+            // 
+            buttonVolver.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            buttonVolver.Location = new Point(391, 52);
+            buttonVolver.Margin = new Padding(45, 3, 3, 25);
+            buttonVolver.Name = "buttonVolver";
+            buttonVolver.Size = new Size(75, 23);
+            buttonVolver.TabIndex = 2;
+            buttonVolver.Text = "VOLVER";
+            buttonVolver.UseVisualStyleBackColor = true;
+            buttonVolver.Click += buttonVolver_Click;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 3;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 46.3461533F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 23.6538467F));
+            tableLayoutPanel4.Controls.Add(label7, 2, 2);
+            tableLayoutPanel4.Controls.Add(label6, 2, 1);
+            tableLayoutPanel4.Controls.Add(label2, 0, 0);
+            tableLayoutPanel4.Controls.Add(label3, 0, 1);
+            tableLayoutPanel4.Controls.Add(label4, 0, 2);
+            tableLayoutPanel4.Controls.Add(textBox1, 1, 0);
+            tableLayoutPanel4.Controls.Add(textBox2, 1, 1);
+            tableLayoutPanel4.Controls.Add(textBox3, 1, 2);
+            tableLayoutPanel4.Controls.Add(label5, 2, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(0, 90);
+            tableLayoutPanel4.Margin = new Padding(0);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 3;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel4.Size = new Size(520, 215);
+            tableLayoutPanel4.TabIndex = 2;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.Red;
+            label7.Location = new Point(402, 167);
+            label7.Margin = new Padding(5, 25, 25, 25);
+            label7.Name = "label7";
+            label7.Size = new Size(25, 23);
+            label7.TabIndex = 8;
+            label7.Text = "*";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.Red;
+            label6.Location = new Point(402, 96);
+            label6.Margin = new Padding(5, 25, 25, 25);
+            label6.Name = "label6";
+            label6.Size = new Size(25, 21);
+            label6.TabIndex = 7;
+            label6.Text = "*";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(90, 25);
+            label2.Margin = new Padding(5, 25, 15, 25);
+            label2.Name = "label2";
+            label2.Padding = new Padding(0, 5, 0, 0);
+            label2.Size = new Size(51, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(90, 96);
+            label3.Margin = new Padding(5, 25, 15, 25);
+            label3.Name = "label3";
+            label3.Padding = new Padding(0, 5, 0, 0);
+            label3.Size = new Size(51, 20);
+            label3.TabIndex = 1;
+            label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Location = new Point(40, 167);
+            label4.Margin = new Padding(5, 25, 15, 25);
+            label4.Name = "label4";
+            label4.Padding = new Padding(0, 5, 0, 0);
+            label4.Size = new Size(101, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Documento (DNI)";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(161, 25);
+            textBox1.Margin = new Padding(5, 25, 5, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(161, 96);
+            textBox2.Margin = new Padding(5, 25, 5, 25);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(231, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(161, 167);
+            textBox3.Margin = new Padding(5, 25, 5, 25);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(231, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(402, 25);
+            label5.Margin = new Padding(5, 25, 25, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 21);
+            label5.TabIndex = 6;
+            label5.Text = "*";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Image = Properties.Resources.Socio;
+            pictureBox1.Location = new Point(10, 0);
+            pictureBox1.Margin = new Padding(10, 0, 0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(270, 450);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // NewAspirante
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "NewAspirante";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "NewAspirante1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            tableLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel3;
+        private Button buttonIngresar;
+        private Button btnLimpiar;
+        private Button buttonVolver;
+        private TableLayoutPanel tableLayoutPanel4;
+        private Label label7;
+        private Label label6;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label5;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ PI_ComB_Grupo2_ClubDeportivo/NewAspirante.cs
@@ -0,0 +1,57 @@
+﻿using PI_ComB_Grupo2_ClubDeportivo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_ComB_Grupo2_ClubDeportivo {
+    public partial class NewAspirante : Form {
+
+        public E_Usuario usuario { set; get; }
+        public NewAspirante() {
+            InitializeComponent();
+        }
+
+        private void buttonIngresar_Click(object sender, EventArgs e) {
+
+            if (textBox1.Text == "" || textBox2.Text == "" ||
+                textBox3.Text == "") {
+                MessageBox.Show("Debe completar datos requeridos (*) ",
+                "AVISO DEL SISTEMA", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            } else {
+                E_Socio socio = new E_Socio();
+                socio.Nombre = textBox1.Text;
+                socio.Apellido = textBox2.Text;
+                socio.DNI = Convert.ToUInt32(textBox3.Text);
+                Datos.Socios dbSocio = new Datos.Socios();
+                if (dbSocio.insertSocio(socio).Length != 29) {
+                    MessageBox.Show("EL SOCIO YA EXISTE", "AVISO DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } else {
+                    MessageBox.Show("Socio Ingresado Correctamente", "AVISO DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                }
+                btnLimpiar_Click(sender,e);
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e) {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.Focus();
+        }
+
+        private void buttonVolver_Click(object sender, EventArgs e) {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.usuario = usuario;
+            mainWindow.Show();
+            this.Hide();
+        }
+    }
+}
