718d80e7aa71a8acf21fb81ea40ca826e038fe22
Lean-Aballone <l.aballone@gmail.com>
2024-10-15 12:52:07 -0300

    Clase usuario, accede a la db para recuperar informacion

---
+++ PI_ComB_Grupo2_ClubDeportivo/Datos/Usuarios.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using PI_ComB_Grupo2_ClubDeportivo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_ComB_Grupo2_ClubDeportivo.Datos {
+    internal class Usuarios {
+
+        public E_Usuario ReadUsuario(string nom, string pass) {
+            E_Usuario salida = new E_Usuario();
+
+            MySqlConnection sqlCon = new MySqlConnection();
+            try {
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand comando = new MySqlCommand("IngresoLogin", sqlCon);
+                comando.CommandType = System.Data.CommandType.StoredProcedure;
+
+                comando.Parameters.Add("Usu", MySqlDbType.VarChar).Value = nom;
+                comando.Parameters.Add("Pass", MySqlDbType.VarChar).Value = pass;
+                sqlCon.Open();
+                MySqlDataReader dr = comando.ExecuteReader();
+                dr.Read();
+                salida.NombreUsu = dr[0].ToString();
+                dr.Read();
+                salida.RolUsu = dr[1].ToString();
+                       
+            }
+            catch (Exception ex) {
+                salida.NombreUsu = "ERROR";
+                salida.RolUsu = ex.Message;
+            }
+            finally {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+                
+            return salida;
+            
+        }
+    }
+}
