435ebcb4875803acae52390d40f73fcd6f8aaf6c
Lean-Aballone <l.aballone@gmail.com>
2024-10-15 12:48:56 -0300

    Entidades SQL

---
+++ PI_ComB_Grupo2_ClubDeportivo/Entidades/E_Socio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_ComB_Grupo2_ClubDeportivo.Entidades {
+    internal class E_Socio {
+        public uint IdSocio { set; get; }
+        public bool activo { set; get; }
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public uint DNI { set; get; }
+        public DateTime FechaIngreso { set; get; }
+
+    }
+}
---
+++ PI_ComB_Grupo2_ClubDeportivo/Entidades/E_Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_ComB_Grupo2_ClubDeportivo.Entidades {
+    public class E_Usuario {
+        public int CodUsu { get; set; }
+        public string? NombreUsu { get; set; }
+        public string? PassUsu { get; set; }
+        public string? RolUsu { get; set; }
+        public bool Activo { get; set; }
+    }
+}
