bc928bee3991e29efcb0d1a5aa39cc270549da00
Lean-Aballone <l.aballone@gmail.com>
2024-11-09 00:40:34 -0300

    Se crea clase Cuota

---
+++ PI_ComB_Grupo2_ClubDeportivo/Entidades/Cuota/Cuota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_ComB_Grupo2_ClubDeportivo.Entidades.Cuota {
+    internal class Cuota {
+        public int Id { get; set; }
+        public int Id_Socio {  get; set; }
+        public bool Vencida { get; set; }
+        public double Monto { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public DateTime FechaPago { get; set; }
+        public bool Pagada { get; set; }
+    }
+}
