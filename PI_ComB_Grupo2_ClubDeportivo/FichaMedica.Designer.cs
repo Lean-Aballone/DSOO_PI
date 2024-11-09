fb9d92d40c7a3ee1462d23946d98bb3e77091526
Lean-Aballone <l.aballone@gmail.com>
2024-11-09 00:28:09 -0300

    Formulario de ficha medica + llamado a guardado en db

---
+++ PI_ComB_Grupo2_ClubDeportivo/FichaMedica.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace PI_ComB_Grupo2_ClubDeportivo {
+    partial class FichaMedica {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 4);
+            tableLayoutPanel1.Controls.Add(button2, 1, 4);
+            tableLayoutPanel1.Controls.Add(checkBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(checkBox2, 1, 1);
+            tableLayoutPanel1.Controls.Add(textBox1, 1, 2);
+            tableLayoutPanel1.Controls.Add(textBox2, 1, 3);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(label4, 0, 3);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(25, 385);
+            button1.Margin = new Padding(25);
+            button1.Name = "button1";
+            button1.Size = new Size(350, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Cargar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.RosyBrown;
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(425, 385);
+            button2.Margin = new Padding(25);
+            button2.Name = "button2";
+            button2.Size = new Size(350, 40);
+            button2.TabIndex = 1;
+            button2.Text = "Volver";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(403, 73);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 2;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(403, 163);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(15, 14);
+            checkBox2.TabIndex = 3;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(403, 200);
+            textBox1.Margin = new Padding(3, 20, 3, 3);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 67);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(403, 290);
+            textBox2.Margin = new Padding(3, 20, 3, 3);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 67);
+            textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(207, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(190, 15);
+            label1.TabIndex = 6;
+            label1.Text = "¿Apto para realizar actividad fisica?";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(247, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 15);
+            label2.TabIndex = 7;
+            label2.Text = "¿Consume Medicamentos?";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(240, 205);
+            label3.Margin = new Padding(3, 0, 3, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(157, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Enfermedades Preexistentes:";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Location = new Point(310, 295);
+            label4.Margin = new Padding(3, 0, 3, 50);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Observaciones:";
+            // 
+            // FichaMedica
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "FichaMedica";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FichaMedica";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ PI_ComB_Grupo2_ClubDeportivo/FichaMedica.cs
@@ -0,0 +1,82 @@
+﻿using PI_ComB_Grupo2_ClubDeportivo.Entidades;
+using PI_ComB_Grupo2_ClubDeportivo.Entidades.Inscripcion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_ComB_Grupo2_ClubDeportivo {
+    public partial class FichaMedica : Form {
+
+        private NewAspirante NewAspirante;
+        private AdministrarSocio AdministrarSocio;
+        private bool SocioExistente;
+        private E_FichaMedica fichaMedica;
+
+        public FichaMedica(uint? dni) {
+            fichaMedica = new E_FichaMedica();
+            fichaMedica.DNI = dni;
+            SocioExistente = true;
+            InitializeComponent();
+        }
+        public FichaMedica(string? nombre, string? apellido ,uint? dni, NewAspirante newAspirante) {
+            NewAspirante = newAspirante;
+            fichaMedica = new E_FichaMedica();
+            fichaMedica.Nombre = nombre;
+            fichaMedica.Apellido = apellido;
+            fichaMedica.DNI = dni;
+            SocioExistente = false;
+            InitializeComponent();
+        }
+
+        public FichaMedica(E_Socio socio, AdministrarSocio administrarSocio) { 
+            AdministrarSocio = administrarSocio;
+            fichaMedica = new E_FichaMedica(socio);
+            SocioExistente = true;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            fichaMedica.PuedeRealizarActividadFisica = checkBox1.Checked;
+            fichaMedica.ConsumeMedicamentos = checkBox2.Checked;
+            fichaMedica.enfPreex = textBox1.Text;
+            fichaMedica.Observaciones = textBox2.Text;
+
+            //Cargar Ficha a base de datos.
+            cargarABase();
+
+            //Regresar
+            if (SocioExistente) {
+                AdministrarSocio.Show();
+            } else { 
+                NewAspirante.MainWindow.Show();
+                NewAspirante.Close();
+            }
+            this.Close();
+        }
+
+        private void cargarABase() {
+            Datos.Socios dbSocio = new Datos.Socios();
+            string rta = dbSocio.insertFicha(fichaMedica);
+            MessageBoxIcon icon = (rta == "Ficha Medica Cargada Correctamente.") ? MessageBoxIcon.Information : MessageBoxIcon.Error;
+            MessageBox.Show(rta, "AVISO DEL SISTEMA", MessageBoxButtons.OK, icon);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.Hide();
+            if (SocioExistente) {    
+                AdministrarSocio.Show();
+                this.Close();
+                return;
+            }
+            NewAspirante.Show();
+            this.Close();
+        }
+    }
+}
