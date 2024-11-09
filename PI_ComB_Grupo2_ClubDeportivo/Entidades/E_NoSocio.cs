dccc056845ffc268d2e43439aebf5fd106c55f83
Lean-Aballone <l.aballone@gmail.com>
2024-11-09 00:36:27 -0300

    Nombre, Apellido y DNI, de NO Socios

---
+++ PI_ComB_Grupo2_ClubDeportivo/Entidades/E_NoSocio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_ComB_Grupo2_ClubDeportivo.Entidades
+{
+    public class E_NoSocio
+    {
+        public string? Nombre { set; get; }
+        public string? Apellido { set; get; }
+        public uint? DNI { set; get; }
+    }
+}
