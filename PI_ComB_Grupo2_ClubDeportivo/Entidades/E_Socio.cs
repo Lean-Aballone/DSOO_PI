79191e60d6642d48b0226e34380e292256c8d0de
Lean-Aballone <l.aballone@gmail.com>
2024-11-09 00:38:43 -0300

    Ahora Socio Hereda de NoSocio, Socio ahora es una clase publica y sus atributos pueden ser null

--- PI_ComB_Grupo2_ClubDeportivo/Entidades/E_Socio.cs
+++ PI_ComB_Grupo2_ClubDeportivo/Entidades/E_Socio.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using PI_ComB_Grupo2_ClubDeportivo.Entidades.Inscripcion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PI_ComB_Grupo2_ClubDeportivo.Entidades {
-    internal class E_Socio {
-        public uint IdSocio { set; get; }
-        public bool activo { set; get; }
-        public string Nombre { set; get; }
-        public string Apellido { set; get; }
-        public uint DNI { set; get; }
-        public DateTime FechaIngreso { set; get; }
+    public class E_Socio : E_NoSocio {
+        public uint? IdSocio { set; get; }
+        public bool? activo { set; get; }
+        public DateTime? FechaIngreso { set; get; }
+        public E_FichaMedica? FichaMedica { set; get; }
 
     }
 }
