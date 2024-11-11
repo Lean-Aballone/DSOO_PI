3535fbcfdcceb03f2229932057b54c2f212a4b83
Lean-Aballone <l.aballone@gmail.com>
2024-11-10 22:16:27 -0300

    Se agrega carnet de socio y la posibilidad de imprimirlo

---
+++ PI_ComB_Grupo2_ClubDeportivo/CarnetSocio.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace PI_ComB_Grupo2_ClubDeportivo {
+    partial class CarnetSocio {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            PanelBtnHide = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanel5 = new TableLayoutPanel();
+            labelNombre = new Label();
+            labelIDSocio = new Label();
+            labelFechaIngreso = new Label();
+            tableLayoutPanel6 = new TableLayoutPanel();
+            label2 = new Label();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            PanelBtnHide.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tableLayoutPanel5.SuspendLayout();
+            tableLayoutPanel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.BackColor = SystemColors.ActiveCaption;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(PanelBtnHide, 0, 1);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 83.3333359F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.666666F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // PanelBtnHide
+            // 
+            PanelBtnHide.BackColor = SystemColors.ActiveCaption;
+            PanelBtnHide.ColumnCount = 2;
+            PanelBtnHide.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            PanelBtnHide.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            PanelBtnHide.Controls.Add(button1, 0, 0);
+            PanelBtnHide.Controls.Add(button2, 1, 0);
+            PanelBtnHide.Dock = DockStyle.Fill;
+            PanelBtnHide.Location = new Point(3, 378);
+            PanelBtnHide.Name = "PanelBtnHide";
+            PanelBtnHide.RowCount = 1;
+            PanelBtnHide.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            PanelBtnHide.Size = new Size(794, 69);
+            PanelBtnHide.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(391, 63);
+            button1.TabIndex = 0;
+            button1.Text = "Imprimir";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(400, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(391, 63);
+            button2.TabIndex = 1;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.AutoSize = true;
+            tableLayoutPanel3.BackColor = SystemColors.ActiveCaption;
+            tableLayoutPanel3.ColumnCount = 1;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Controls.Add(tableLayoutPanel2, 0, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(0, 0);
+            tableLayoutPanel3.Margin = new Padding(0);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(800, 375);
+            tableLayoutPanel3.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.BackColor = SystemColors.Window;
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel4, 0, 1);
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel6, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(20, 20);
+            tableLayoutPanel2.Margin = new Padding(20);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 25.3731346F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 74.62687F));
+            tableLayoutPanel2.Size = new Size(760, 335);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 2;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 28.51459F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 71.48541F));
+            tableLayoutPanel4.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel4.Controls.Add(tableLayoutPanel5, 1, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(3, 88);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Size = new Size(754, 244);
+            tableLayoutPanel4.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Window;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.Socio;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(215, 244);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel5
+            // 
+            tableLayoutPanel5.ColumnCount = 1;
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel5.Controls.Add(labelNombre, 0, 0);
+            tableLayoutPanel5.Controls.Add(labelIDSocio, 0, 2);
+            tableLayoutPanel5.Controls.Add(labelFechaIngreso, 0, 1);
+            tableLayoutPanel5.Dock = DockStyle.Fill;
+            tableLayoutPanel5.Location = new Point(218, 3);
+            tableLayoutPanel5.Name = "tableLayoutPanel5";
+            tableLayoutPanel5.RowCount = 3;
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel5.Size = new Size(533, 238);
+            tableLayoutPanel5.TabIndex = 4;
+            // 
+            // labelNombre
+            // 
+            labelNombre.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelNombre.AutoSize = true;
+            labelNombre.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            labelNombre.Location = new Point(3, 0);
+            labelNombre.Name = "labelNombre";
+            labelNombre.Size = new Size(527, 30);
+            labelNombre.TabIndex = 0;
+            labelNombre.Text = "label1";
+            // 
+            // labelIDSocio
+            // 
+            labelIDSocio.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelIDSocio.AutoSize = true;
+            labelIDSocio.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelIDSocio.Location = new Point(3, 158);
+            labelIDSocio.Name = "labelIDSocio";
+            labelIDSocio.Size = new Size(527, 21);
+            labelIDSocio.TabIndex = 1;
+            labelIDSocio.Text = "Socio N°: ";
+            // 
+            // labelFechaIngreso
+            // 
+            labelFechaIngreso.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelFechaIngreso.AutoSize = true;
+            labelFechaIngreso.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelFechaIngreso.Location = new Point(3, 79);
+            labelFechaIngreso.Name = "labelFechaIngreso";
+            labelFechaIngreso.Size = new Size(527, 21);
+            labelFechaIngreso.TabIndex = 2;
+            labelFechaIngreso.Text = "Ingreso: ";
+            // 
+            // tableLayoutPanel6
+            // 
+            tableLayoutPanel6.ColumnCount = 1;
+            tableLayoutPanel6.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel6.Controls.Add(label2, 0, 0);
+            tableLayoutPanel6.Controls.Add(label1, 0, 1);
+            tableLayoutPanel6.Dock = DockStyle.Fill;
+            tableLayoutPanel6.Location = new Point(0, 0);
+            tableLayoutPanel6.Margin = new Padding(0);
+            tableLayoutPanel6.Name = "tableLayoutPanel6";
+            tableLayoutPanel6.RowCount = 2;
+            tableLayoutPanel6.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel6.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel6.Size = new Size(760, 85);
+            tableLayoutPanel6.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.Font = new Font("Segoe UI", 24F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            label2.Location = new Point(0, 0);
+            label2.Margin = new Padding(0);
+            label2.Name = "label2";
+            label2.Size = new Size(760, 42);
+            label2.TabIndex = 2;
+            label2.Text = "CLUB DEPORTIVO";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(5, 47);
+            label1.Margin = new Padding(5, 5, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Carnet de socio ";
+            // 
+            // CarnetSocio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "CarnetSocio";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Carnet de Socio";
+            Load += CarnetSocio_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            PanelBtnHide.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tableLayoutPanel5.ResumeLayout(false);
+            tableLayoutPanel5.PerformLayout();
+            tableLayoutPanel6.ResumeLayout(false);
+            tableLayoutPanel6.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel PanelBtnHide;
+        private Button button1;
+        private Button button2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel4;
+        private PictureBox pictureBox1;
+        private TableLayoutPanel tableLayoutPanel5;
+        private Label labelNombre;
+        private Label labelIDSocio;
+        private Label labelFechaIngreso;
+        private TableLayoutPanel tableLayoutPanel6;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ PI_ComB_Grupo2_ClubDeportivo/CarnetSocio.cs
@@ -0,0 +1,72 @@
+﻿using PI_ComB_Grupo2_ClubDeportivo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_ComB_Grupo2_ClubDeportivo {
+    public partial class CarnetSocio : Form {
+
+        private AdministrarSocio administrarSocio;
+        private E_Socio socio;
+        public CarnetSocio(E_Socio socio, AdministrarSocio administrarSocio) {
+            this.socio = socio;
+            InitializeComponent();
+            this.administrarSocio = administrarSocio;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            regresar();
+        }
+
+        private void CarnetSocio_Load(object sender, EventArgs e) {
+            labelNombre.Text = socio.Nombre.ToUpper() + " " + socio.Apellido.ToUpper();
+            labelFechaIngreso.Text += Convert.ToString(socio.FechaIngreso);
+            labelIDSocio.Text += Convert.ToString(socio.IdSocio);
+            label1.Text += DateTime.UtcNow.ToShortDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            PanelBtnHide.Visible = false;
+            imprimirCarnet();
+            PanelBtnHide.Visible = true;
+            regresar();
+        }
+        private void imprimirCarnet() {
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(ImprimirForm1);
+            PrintDialog pdi = new PrintDialog();
+            pdi.Document = pd;
+            if (pdi.ShowDialog() == DialogResult.OK) {
+                pd.Print();
+                MessageBox.Show("Operación existosa", "AVISO DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else {
+                MessageBox.Show("Impresión cancelada", "AVISO DEL SISTEMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void ImprimirForm1(object o, PrintPageEventArgs e) {
+            int x = SystemInformation.WorkingArea.X;
+            int y = SystemInformation.WorkingArea.Y;
+            int ancho = this.Width;
+            int alto = this.Height;
+            Rectangle bounds = new Rectangle(x, y, ancho, alto);
+            Bitmap img = new Bitmap(ancho, alto);
+            this.DrawToBitmap(img, bounds);
+            Point p = new Point(0, 100);
+            e.Graphics.DrawImage(img, p);
+        }
+
+        private void regresar() {
+            administrarSocio.Show();
+            this.Close();
+        }
+    }
+}
