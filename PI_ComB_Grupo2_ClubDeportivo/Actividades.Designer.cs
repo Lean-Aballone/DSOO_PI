a518556efcbded0bc530fb0def1748a5a36d13c7
Lean-Aballone <l.aballone@gmail.com>
2024-11-14 16:21:25 -0300

    Ventana para inscribir en actividad

---
+++ PI_ComB_Grupo2_ClubDeportivo/Actividades.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PI_ComB_Grupo2_ClubDeportivo {
+    partial class Actividades {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Actividades));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(button2, 3, 0);
+            tableLayoutPanel1.Controls.Add(button3, 3, 1);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 2, 0);
+            tableLayoutPanel1.Controls.Add(listBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.MediumSpringGreen;
+            button2.Dock = DockStyle.Fill;
+            button2.Enabled = false;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(625, 25);
+            button2.Margin = new Padding(25);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 175);
+            button2.TabIndex = 5;
+            button2.Text = "Inscribir";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.RosyBrown;
+            button3.Dock = DockStyle.Fill;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(625, 250);
+            button3.Margin = new Padding(25);
+            button3.Name = "button3";
+            button3.Size = new Size(150, 175);
+            button3.TabIndex = 4;
+            button3.Text = "Volver";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(400, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            tableLayoutPanel1.SetRowSpan(pictureBox1, 2);
+            pictureBox1.Size = new Size(200, 450);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 30;
+            listBox1.Items.AddRange(new object[] { "Actividad1", "Actividad2", "Actividad3", "Actividad4", "Actividad5" });
+            listBox1.Location = new Point(225, 30);
+            listBox1.Margin = new Padding(25, 30, 25, 30);
+            listBox1.Name = "listBox1";
+            tableLayoutPanel1.SetRowSpan(listBox1, 2);
+            listBox1.Size = new Size(150, 390);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += actividadSeleccionada;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.Location = new Point(25, 30);
+            label1.Margin = new Padding(25, 30, 25, 30);
+            label1.Name = "label1";
+            tableLayoutPanel1.SetRowSpan(label1, 2);
+            label1.Size = new Size(150, 390);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Visible = false;
+            // 
+            // Actividades
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FloralWhite;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Actividades";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Actividades";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private ListBox listBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button3;
+        private Button button2;
+    }
+}
---
+++ PI_ComB_Grupo2_ClubDeportivo/Actividades.cs
@@ -0,0 +1,102 @@
+﻿using MySql.Data.MySqlClient;
+using PI_ComB_Grupo2_ClubDeportivo.Datos;
+using PI_ComB_Grupo2_ClubDeportivo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_ComB_Grupo2_ClubDeportivo {
+    public partial class Actividades : Form {
+        E_Socio socio;
+        Image original;
+        Form form;
+        string actividad;
+        public Actividades(Form form) {
+            this.form = form;
+
+            MenuDeSocio menuDeSocio = new MenuDeSocio(this);
+            this.Hide();
+
+            InitializeComponent();
+            
+            original = pictureBox1.Image;
+            pictureBox1.Image = MakeGrayscale((Bitmap)pictureBox1.Image);
+
+        }
+
+
+        public void setLabel(E_Socio socio) {
+            this.socio = socio;
+            label1.Text = socio.Nombre + " " + socio.Apellido + 
+                "\nEstado: " + (((bool)socio.activo) ? "activo" : "Inactivo") +
+                "\nFicha Medica: " + ((socio.FichaMedica is not null) ? "Si" : "No");
+            label1.Visible = true;
+        }
+        private void button3_Click(object sender, EventArgs e) {
+            form.Show();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            string query = "INSERT INTO actividad_socio (IdActividad, IdSocio) VALUES (" + 
+                (listBox1.SelectedIndex + 1).ToString() +
+                "," + socio.IdSocio + ");";
+            string rta;
+            MySqlConnection sqlCon = new MySqlConnection();
+            try {
+                sqlCon = Conexion.getInstancia().CrearConexion();
+                MySqlCommand comando = new MySqlCommand(query, sqlCon);
+                comando.CommandType = CommandType.Text;
+                sqlCon.Open();
+                rta = (comando.ExecuteNonQuery() > 0) ? "Operación realizada exitosamente" : "Hubo un error";
+            }
+            catch (Exception ex) {
+                rta = ex.Message;
+                MessageBox.Show(rta);
+            }
+            finally {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+            MessageBox.Show(rta);
+            if (rta == "Operación realizada exitosamente") {
+                button2.Enabled = true;
+                button2.BackColor = (button2.Enabled) ? Color.MediumSpringGreen : Color.Gray;
+            }
+        }
+
+        public Bitmap MakeGrayscale(Bitmap original) {
+            Bitmap newBmp = new Bitmap(original.Width, original.Height);
+            Graphics g = Graphics.FromImage(newBmp);
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                   new float[] {.3f, .3f, .3f, 0, 0},
+                   new float[] {.59f, .59f, .59f, 0, 0},
+                   new float[] {.11f, .11f, .11f, 0, 0},
+                   new float[] {0, 0, 0, 1, 0},
+                   new float[] {0, 0, 0, 0, 1}
+               });
+            ImageAttributes img = new ImageAttributes();
+            img.SetColorMatrix(colorMatrix);
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height), 0, 0, original.Width, original.Height, GraphicsUnit.Pixel, img);
+            g.Dispose();
+            return newBmp;
+        }
+
+        private void actividadSeleccionada(object sender, EventArgs e) {
+            if ((bool)socio.activo && socio.FichaMedica is not null) { 
+                 actividad = listBox1.SelectedItem.ToString(); 
+                  pictureBox1.Image = original;
+                   button2.Enabled = true;
+            }
+            
+        }
+    }
+}
