b517b61ba911734283aecee17827914b08a34863
Lean-Aballone <l.aballone@gmail.com>
2024-11-16 15:10:13 -0300

    Ventana permite seleccionar si el tramite es de socio o no socio

---
+++ PI_ComB_Grupo2_ClubDeportivo/SocioNoSocio.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace PI_ComB_Grupo2_ClubDeportivo {
+    partial class SocioNoSocio {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button2 = new Button();
+            button1 = new Button();
+            button3 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(button2, 1, 0);
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button3, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 67.77778F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 32.22222F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 248, 201);
+            button2.Dock = DockStyle.Fill;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(450, 65);
+            button2.Margin = new Padding(50, 65, 50, 50);
+            button2.Name = "button2";
+            button2.Size = new Size(300, 190);
+            button2.TabIndex = 6;
+            button2.Text = "No Socio";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(242, 225, 136);
+            button1.Dock = DockStyle.Fill;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(50, 65);
+            button1.Margin = new Padding(50, 65, 50, 50);
+            button1.Name = "button1";
+            button1.Size = new Size(300, 190);
+            button1.TabIndex = 5;
+            button1.Text = "Socio";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.RosyBrown;
+            tableLayoutPanel1.SetColumnSpan(button3, 2);
+            button3.Dock = DockStyle.Fill;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(25, 330);
+            button3.Margin = new Padding(25);
+            button3.Name = "button3";
+            button3.Size = new Size(750, 95);
+            button3.TabIndex = 4;
+            button3.Text = "Volver";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // SocioNoSocio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FloralWhite;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "SocioNoSocio";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SocioNoSocio";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button3;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ PI_ComB_Grupo2_ClubDeportivo/SocioNoSocio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_ComB_Grupo2_ClubDeportivo {
+    public partial class SocioNoSocio : Form {
+
+        Form form;
+
+        public SocioNoSocio(Form form) {
+            this.form = form;
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            form.Show();
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            MenuDeSocio menuDeSocio = new MenuDeSocio(this);
+            menuDeSocio.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Actividades actividades = new Actividades(form);
+            actividades.Show();
+            this.Close();
+        }
+    }
+}
